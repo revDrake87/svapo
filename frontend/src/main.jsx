10ecf4bf86345fa41a90dc7cc1f557fca4b61e8b
revDrake87 <john8795@gmail.com>
2026-06-17 18:49:59 +0200

    feat: downgrade Tailwind for Dark Mode fix and add image upload instructions

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
-import '@fontsource/inter'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import '@fontsource/inter';
 import './index.css'
 import App from './App.jsx'
 
