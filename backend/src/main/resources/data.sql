2f984df760bb9117e9ec6fa1cf1d0fcd950e2201
revDrake87 <john8795@gmail.com>
2026-07-01 09:35:05 +0200

    update with empty db and new credentials for admin users

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,4 @@
-INSERT INTO store_settings (id, store_name) VALUES (1, 'VapeStore');
+INSERT INTO store_settings (id, store_name) VALUES (1, 'Professional Vape');
 
-INSERT INTO users (username, password) VALUES ('admin', '$2b$10$PaBJEcYVA6/DcNoSEc7GKO/Fq0SXPEOi7A3CUizMBCdurImbd3L7a');
-
-
+INSERT INTO users (username, password) VALUES ('admin_prof', '$2b$12$wMY/LJrE5MgIIW8oknuhW.Sqm3RlkNU9wdCcj9Pa6MSiZbytv6kD6');
+INSERT INTO users (username, password) VALUES ('admin_puff', '$2b$12$UNXUcdwq0QOVTHiMynkKJeHhwgyhyb7Hs1VHFldzh8Ze6CXkYBHya');
--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,13 +1,3 @@
 export const getApiUrl = () => {
-  // Se siamo in locale (localhost o IP della LAN), usiamo il percorso relativo o localhost
-  if (
-    window.location.hostname === "localhost" || 
-    window.location.hostname === "127.0.0.1" || 
-    window.location.hostname.startsWith("192.168.")
-  ) {
-    return "/api"; 
-  }
-
-  // IN PRODUZIONE: Forza l'URL reale del tuo backend su Railway
-  return "https://svapo-production.up.railway.app/api";
+  return `/api`;
 };
